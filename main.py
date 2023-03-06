6bd05be1c9830107073f40e4a7f768ce51878ae0
Herma <107596@student.unis.no>
2023-03-06 11:07:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import unisacsi.Ocean as Oc
+import unisacsi.Meteo as Met
+from unisacsi.MET_model_download import download_MET_model_static_fields, download_MET_model_data
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import cartopy.crs as ccrs
+import gsw
+import cmocean as cmo
+import windrose
+import xarray as xr
+import seaborn as sns
+import spectrum
+from scipy import signal
